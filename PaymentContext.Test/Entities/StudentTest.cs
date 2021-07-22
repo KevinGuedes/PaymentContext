a209b9db1c8ab3458abb4bd22eaa793aa0402a8d
Kevin Guedes <kevinguedes@edu.unifor.br>
2021-07-21 21:40:01 -0300

    Unit tests for document value object

---
+++ PaymentContext.Test/Entities/StudentTest.cs
@@ -0,0 +1,43 @@
+﻿using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PaymentContext.Test.Entities
+{
+    public class StudentTest
+    {
+        public StudentTest()
+        {
+            CreateValidStudent();
+        }
+
+        public Student ValidStudent { get; set; }
+
+        private void CreateValidStudent()
+        {
+            var name = new Name("Kevin", "Guedes");
+            var email = new Email("hello@gmail.com");
+            var document = new Document("98989698368", EDocumentType.CPF);
+
+            ValidStudent = new Student(name, document, email);
+        }
+
+        [Fact]
+        public void ShouldReturnErrorWhenStudentNameIsInvalid()
+        {
+            var name = new Name("Kn", "Guedes");
+            var email = new Email("hello@gmail.com");
+            var document = new Document("98989698368", EDocumentType.CPF);
+
+            var student = new Student(name, document, email);
+
+            Assert.False(student.IsValid);
+        }
+    }
+}
---
+++ PaymentContext.Test/ValueObjects/DocumentTest.cs
@@ -0,0 +1,50 @@
+﻿using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PaymentContext.Test.ValueObjects
+{
+    public class DocumentTest
+    {
+        //Red, Green, Refactor
+
+        [Fact]
+        public void ShouldReturnErrorWhenCNPJIsInvalid()
+        {
+            var document = new Document("123", EDocumentType.CNPJ);
+
+            Assert.False(document.IsValid);
+        }
+
+        [Fact]
+        public void ShouldReturnSuccessWhenCNPJIsValid()
+        {
+            var document = new Document("01562124000123", EDocumentType.CNPJ);
+
+            Assert.True(document.IsValid);
+        }
+
+        [Theory]
+        [InlineData("123")]
+        [InlineData("321")]
+        public void ShouldReturnErrorWhenCPFIsInvalid(string cpf)
+        {
+            var document = new Document(cpf, EDocumentType.CPF);
+
+            Assert.False(document.IsValid);
+        }
+
+        [Fact]
+        public void ShouldReturnSuccessWhenCPFIsValid()
+        {
+            var document = new Document("68963236987", EDocumentType.CPF);
+
+            Assert.True(document.IsValid);
+        }
+    }
+}
